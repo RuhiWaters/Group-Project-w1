2756dde419be5d59961ef230c13d21d450f2c4cd
Ru <edel.m.waters@gmail.com>
2023-11-13 14:19:23 -0600

    Finished client.js

--- scripts/client.js
+++ scripts/client.js
@@ -1,57 +1,63 @@
-console.log('JS sourced');
+onsole.log('JS sourced');
 
-let cities = 'Minneapolis, St.Paul, Tulsa'; 
-console.log(Array.isArray(cities));
+//### Activity
 
+//- [ ] Create an `index.html`, `style.css` and `client.js` file.
+
+
+//- [ ] Create a string of city names representing the city of each group member separated by a comma.
+let cities = 'Minneapolis, St. Paul, Tulsa';
+console. log(cities)
+//```
+//let cities = 'Minneapolis, St. Paul, etc.';
+//```
+
+//- [ ] Console log whether or not the variable cities is an array (use the built in method from above).
+console.log (Array.isArray(cities));
+
+//- [ ] Turn the cities into an array using one of the built in methods above.
 let citiesArray = cities.split(',');
-console.log (citiesArray); 
-
-
+console.log(citiesArray);
+//```
+//let citiesArray = cities.???;
+//```
+//- [ ] Loop over each city and append to the dom with `innerHTML`
 let citiesElement = document.querySelector('#studentCities');
-console. log('Elements:', citiesElement); // ('Element', + ) can be used too?
-
-for (let city of citiesArray) {
-    citiesElement.innerHTML += '<li>${city}</li>'; //p instead of li?
-}
-
-let containsMinneapolis = false;
+console.log('ELEMENT:', citiesElement);
+//- [ ] Check whether or not your array contains 'Minneapolis' using a `for loop`
+let containsMin = false;
+//- [ ] Check whether or not your array contains 'Minneapolis' using a build in method
 for (let x = 0; x < citiesArray.length; x++) {
-    if (citiesArray[x] === 'Minneapolis'); {
-        containsMinneapolis = true;
+    if (citiesArray[x] === 'Minneapolis') {
+        containsMin = true;
         break;
-    }
-    //break?
+    } 
 }
-console.log('Contains Minneapolis: ',  containsMinneapolis);// + or (', )
+console.log('containsMin (looped):', containsMin);
+//### Stretch Goals
 
-
-containsMinneapolis = false; //build in method
-if (citiesArray.lastIndexOf('Minneapolis') != -1) {
-    containsMinneapolis = true;
-} else {
-    containsMinneapolis = false;
-}
-console.log('containsMinneapolis (build in method):', containsMinneapolis);
-
+//- [ ] Remove white space around each of the cities (e.g 'St. Paul' instead of ' St. Paul')
 for (let x = 0; x < citiesArray.length; x++) {
-    citiesArray[x] = citiesArray[x].trim ();
-}   
-    console.log (citiesArray[x]);   
-    console. log('Trim spacing in citiesArray',  citiesArray);
+    console.log(citiesArray[x]);
+    citiesArray[x] = citiesArray[x].trim();
+    console.log(citiesArray[x]);
+   }
+   console.log('Trimmed citiesArray:', citiesArray);
+//- [ ] Remove duplicate cities
+citiesArray.push('Minneapolis'); 
+citiesArray.push('Tulsa');
+console.log('citiesArray before uniqueness processing:', citiesArray)
 
-    //duplicate cities?
-    
-    citiesArray.push('Minneapolis');
-    citiesArray.push('Tulsa');
-    console. log('citiesArray before uniqueness change', citiesArray);
+//uniqueCities
 
-    let uniqueCities =[];
-    let x = citiesArray
-    for (let x of citiesArray) {
-        if (uniqueCities.lastIndexOf(x) === -1); {
-            uniqueCities.push(x);
-         } // astindexof(x)?
+let uniqueCities = [];
+
+for (let x of citiesArray) {
+    if (uniqueCities.lastIndexOf(x) === -1) {
+        uniqueCities.push(x);
     }
-    console. log('uniqueCities',  uniqueCities);
-    citiesArray = uniqueCities;
-    console. log('citiesArray changed:', citiesArray);
+}
+citiesArray = uniqueCities;
+console.log('citiesArray after reassignment:', citiesArray);
+
+//
